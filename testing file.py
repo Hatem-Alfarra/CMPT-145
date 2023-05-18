5854faae70bd348888f17dd65ee47dd650818f1b
Hatem Alfarra <hma194@Usask.ca>
2023-05-17 23:04:59 -0400

    Lots of changes to all be deleted later. Script 1 and 2 from day 2 lectures with time taken to run code added. Both scripts find the number of prime numbers in a range.

--- testing file.py
+++ testing file.py
@@ -9,8 +9,9 @@ t0 = t.time()
 
 
 # Bad slow code as an example in the textbook (script 1)
+# inefficient
 
-n = 1000
+n = 10000
 count = 0
 for i in range(2,n + 1):
     no_factors_found = True
@@ -30,36 +31,41 @@ t1 = t.time()
 print(t1 - t0)
 
 
+# Faster code (better)
+# Does it work? yes
+ti =t.time()
 
+n = 10000
 
-# n = 12 # end of range of numbers to check for primes
-#
-# count = 0
-# for i in range (2 , n + 1):
-#     no_factors_found = True # assume prime until disproven
-#     f = 2
-#     # check if i is prime by checking remainders
-#     while no_factors_found and f < i :
-#         if i % f == 0:
-#             no_factors_found = False
-#         f += 1
-# if no_factors_found:
-#     count += 1
-#
-# print ("# Prime numbers between 2 and " + str (n ) + ":", count )
+still_is_prime = ( n +1)*[ True ]
 
+for i in range (2 , n):
+    if still_is_prime [ i ]:
+        j = 2* i
+        while j <= n :
+            still_is_prime [ j ] = False
+            j += i
 
-# n = 10
+count = sum([1 for v in still_is_prime[2:] if v])
+
+print(" # Prime numbers between 2 and " + str(n) + ":", count)
+
+tf = t.time()
+
+print(tf-ti)
+
+# n = 20
 #
-# count = 0
-# for potential_prime in range(2, n+1):
-#     not_factorable = True
-#     potential_comp = 2
-#     while not_factorable and potential_comp < potential_prime:
-#         if potential_prime % potential_comp == 0:
-#             not_factorable = False
-#             potential_comp += 1
-#     if not_factorable:
-#         count += 1
+# still_is_prime = ( n +1)*[ True ]
 #
-# print(count)
+# for i in range (2 , n ):
+#     if still_is_prime [ i ]:
+#         j = 2* i
+#         while j <= n :
+#             still_is_prime [ j ] = False
+#             j += i
+#
+#
+# count = sum ([1 for v in still_is_prime [2:] if v ])
+#
+# print ( " # Prime numbers between 2 and " + str ( n ) + " : " , count )
