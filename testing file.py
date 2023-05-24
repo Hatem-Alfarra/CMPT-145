9e56cfbdcf3f70d4be728703e82ea34380ccd9ea
Hatem Alfarra <hma194@Usask.ca>
2023-05-24 00:03:11 -0400

    While loop vs. built-in sorted function timed for sorting numbers in a list

--- testing file.py
+++ testing file.py
@@ -2,14 +2,15 @@
 #
 # print('Hello Git')
 # print('Hello Git on Pycharm')
-
-import time as t
+import time
 import random as r
-
 
 list = list()
 
-n = 10
+
+t1 = time.time()
+
+n = 100000
 
 while n > 0:
     num = r.randint(1,9)
@@ -18,11 +19,28 @@ while n > 0:
     n -= 1
 
 
-new_list = []
+t2 = time.time()
+
+new_list_sorted = sorted(list)
+t3 = time.time()
+
+new_list_looped = []
 
 while len(list) > 0:
     small_num = min(list)
     list.remove(small_num)
-    new_list.append(small_num)
+    new_list_looped.append(small_num)
 
-print(new_list)
+t4 = time.time()
+
+
+tl = t2 - t1
+tsorted = t3 - t2
+tloop = t4 - t3
+
+
+print(new_list_looped)
+print(new_list_sorted)
+print('new list made in: ' + str(tl))
+print('Sorted in: ' + str(tloop), 'using the while loop')
+print('Sorted in: ' + str(tsorted), 'using the the sort method')
