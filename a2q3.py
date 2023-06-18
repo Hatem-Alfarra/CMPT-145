6048d8667472ef0f734e136ea2296665fa34c0f9
Hatem Alfarra <hma194@Usask.ca>
2023-06-18 12:56:12 -0400

    A2Q3 - copy_list_of_lists() changed to extract the data from the original list rather than use the same reference

--- a2q3.py
+++ a2q3.py
@@ -36,7 +36,11 @@ def copy_list_of_lists(data: list) -> list:
         list - a new list with new internal lists created by copying the
      information from the list passed in.
     """
-    new_list = data
+
+    # CHANGE:
+    # the slice operator [:] creates a new list containing all the elements from the original list
+    # new_list = data
+    new_list = data[:]
     return new_list
 
 def copy_dict_of_dicts(data: dict) -> dict:
